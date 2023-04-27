f31dde8e262f0ec64b80c02806e6137df9b1691f
Amina <aminabakkali2001@gmail.com>
2023-04-27 19:33:35 +0100

    signup error fixed

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,19 +3,53 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 
 class RegistrationController extends AbstractController
 {
+
+    #[Route('/register', name: 'app_register', methods: ['POST'])]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        // Get the JSON payload from the request body
+        $data = json_decode($request->getContent(), true);
+
+        // Create a new user entity
+        $user = new User();
+        $user->setUsername($data['username']);
+        $user->setFirstName($data['first_name']);
+        $user->setLastName($data['last_name']);
+        $user->setEmail($data['email']);
+
+        // Encode the password and set it on the user entity
+        $password = $userPasswordHasher->hashPassword($user, $data['password']);
+        $user->setPassword($password);
+
+        // Persist the user entity to the database
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // Return a success JSON response
+        return $this->json([
+            'status' => 'success',
+            'message' => 'User registered successfully',
+            'user' => [
+                'username' => $user->getUsername(),
+                'email' => $user->getEmail(),
+                'first_name' => $user->getFirstName(),
+                'last_name' => $user->getLastName(),
+            ],
+        ]);
+    }
+
+    /*
     #[Route('/register', name: 'app_register')]
 public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
 {
@@ -62,6 +96,5 @@ public function register(Request $request, UserPasswordHasherInterface $userPass
     return $this->render('registration/register.html.twig', [
         'registrationForm' => $form->createView(),
     ]);
-}
-
+}*/
 }
