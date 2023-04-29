91e8c14e5ad7688beaee42265732e2f636854026
hindehalhoul <hind.halhoul.2003@gmail.com>
2023-04-29 14:48:03 +0200

    delete not the final one

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class CartController extends AbstractController
 {
@@ -29,6 +30,31 @@ class CartController extends AbstractController
             'message' => 'Cart fetched successfully',
             'data' => $data
         ]);
+    }
+}
+#[Route('/cart/delete/{id}', name: 'cart_delete', methods: ['DELETE'])]
+public function delete(Request $request, int $id): JsonResponse
+{
+    $cart = $request->cookies->get('cart', '{}');
+    $cart = json_decode($cart, true);
+
+    if (isset($cart[$id])) {
+        unset($cart[$id]);
+        $cart = json_encode($cart);
+
+        // Create a new cookie with the updated cart data and the same expiration time
+        $response = new JsonResponse([
+            'status' => 'success',
+            'message' => 'Product removed from cart successfully'
+        ]);
+        $response->headers->setCookie(new Cookie('cart', $cart, strtotime('+1 year')));
+
+        return $response;
+    } else {
+        return new JsonResponse([
+            'status' => 'error',
+            'message' => 'Product not found in cart'
+        ], 404);
     }
 }
 
